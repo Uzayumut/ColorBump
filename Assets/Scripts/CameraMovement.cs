a870dd775bc59168ef19a2157d5e5eb142cf8a49
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-18 17:13:10 +0300

    Updated CameraMovement.cs(camVelocity)

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public float cameraSpeed = 6;
+    public Vector3 camVelocity;
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.forward * cameraSpeed * Time.deltaTime;
+        camVelocity= Vector3.forward * cameraSpeed * Time.deltaTime;
     }
 }
