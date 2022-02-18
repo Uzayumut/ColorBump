d097c488728747118eae69883897198ef82cd586
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-18 18:28:41 +0300

    Added MoveLeftAndRight.cs

---
+++ Assets/Scripts/MoveLeftAndRight.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftAndRight : MonoBehaviour
+{
+    public float speed;
+    private float minX, maxX;
+
+    public bool right, dontMove;
+    private bool stop;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
