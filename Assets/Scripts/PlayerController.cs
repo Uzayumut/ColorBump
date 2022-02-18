696886cf3190c3fe3a89a0089b128f1e4ab7882f
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-18 16:25:22 +0300

    Updated PlayerController.cs(Input, mousePosition,MoveForce)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,14 +9,37 @@ public class PlayerController : MonoBehaviour
     public float sensitivity = .16f, clampDelta = 42f;
 
     public float bounds = 5;
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMousePos = Input.mousePosition;
+            
+
+        }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            
+            Vector3 vector = lastMousePos - Input.mousePosition;
+            lastMousePos = Input.mousePosition;
+            vector = new Vector3(vector.x, 0, vector.y);
+
+            Vector3 moveForce = Vector3.ClampMagnitude(vector, clampDelta);
+            
+            rb.AddForce(Vector3.forward * 2+(-moveForce*sensitivity-rb.velocity/5), ForceMode.VelocityChange);
+            //A forward direction(Vector3.forward*2)
+            //independent of the weight of the mass (ForceMod.VelocityChange)
+            //mouse not moving backwards(-moveforce)
+            //added rigidbody change rate and precision(*sensitivity-rb.velocity/5)
+
+        }
     }
 }
