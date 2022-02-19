288c20bf11ead8d8ce52696de46d5d762643d098
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-19 18:35:36 +0300

    Updated PlayerController.cs(canMove)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     public float sensitivity = .16f, clampDelta = 42f;
 
     public float bounds = 5;
+
+    [HideInInspector]
+    public bool canMove;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -16,7 +19,15 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -bounds, bounds), transform.position.y,transform.position.z);//Clamps the given value between the given minimum float and maximum float values
-        transform.position += FindObjectOfType<CameraMovement>().camVelocity;
+        if(canMove)
+            transform.position += FindObjectOfType<CameraMovement>().camVelocity;
+
+        if (!canMove)
+        {
+            if (Input.GetMouseButtonDown(0))
+                canMove = true;
+        }
+
     }
 
 
@@ -28,24 +39,28 @@ public class PlayerController : MonoBehaviour
             
 
         }
-
-
-        if (Input.GetMouseButton(0))
+        if (canMove)
         {
-            
-            Vector3 vector = lastMousePos - Input.mousePosition;
-            lastMousePos = Input.mousePosition;
-            vector = new Vector3(vector.x, 0, vector.y);
+            if (Input.GetMouseButton(0))
+            {
 
-            Vector3 moveForce = Vector3.ClampMagnitude(vector, clampDelta);
-            
-            rb.AddForce(-moveForce*sensitivity-rb.velocity/5, ForceMode.VelocityChange);
-            //A forward direction(Vector3.forward*2)
-            //independent of the weight of the mass (ForceMod.VelocityChange)
-            //mouse not moving backwards(-moveforce)
-            //added rigidbody change rate and precision(*sensitivity-rb.velocity/5)
+                Vector3 vector = lastMousePos - Input.mousePosition;
+                lastMousePos = Input.mousePosition;
+                vector = new Vector3(vector.x, 0, vector.y);
 
+                Vector3 moveForce = Vector3.ClampMagnitude(vector, clampDelta);
+
+                rb.AddForce(-moveForce * sensitivity - rb.velocity / 5, ForceMode.VelocityChange);
+                //A forward direction(Vector3.forward*2)
+                //independent of the weight of the mass (ForceMod.VelocityChange)
+                //mouse not moving backwards(-moveforce)
+                //added rigidbody change rate and precision(*sensitivity-rb.velocity/5)
+
+            }
         }
+
+
+       
 
         rb.velocity.Normalize();
 
