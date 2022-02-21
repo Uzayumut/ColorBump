6849bb828dfd518d5703a9f3d75b91c740ecc907
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-21 16:06:13 +0300

    Updated. GameManager.cs (variables)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Text cLevelText, nLevelText;
+    private Image fill;
+
+    private float startDistance, distance;
+
+    private GameObject player, finish, levelNo;
     void Start()
     {
         
